822f162193dfa1e7953bb6a1f61d6803403ddbf4
Arsen Prohorchik <arsenpr2011@gmail.com>
2014-09-04 01:40:19 +0300

    add on esc hiding

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,61 +1,61 @@
 (function( $ ) {
-	'use strict';
-	var comboboxParent = $('.combobox');
+    'use strict';
+    var comboboxParent = $('.combobox');
 
-	function buildOptionList(opts) {
-		var list = $('<ul role="menu" aria-labelledby="dLabel">')
-		.addClass('dropdown-menu options');
-		opts.forEach(function(item, i) {
-			$('<li>' + item + '</li>')
-			.addClass('combobox-option')
-			.attr('tabindex', i + 1)
-			.appendTo(list).click(onOptionClick);
-		});
+    function buildOptionList(opts) {
+        var list = $('<ul role="menu" aria-labelledby="dLabel">')
+        .addClass('dropdown-menu options');
+        opts.forEach(function(item, i) {
+            $('<li>' + item + '</li>')
+            .addClass('combobox-option')
+            .attr('tabindex', i + 1)
+            .appendTo(list).click(onOptionClick);
+        });
 
-		list.appendTo(comboboxParent);
-	}
+        list.appendTo(comboboxParent);
+    }
 
-	function onOptionClick() {
-		var text = $(this).text();
-		var input = $('.combobox-input');
-		input.val(text);
-	}
+    function onOptionClick() {
+        var text = $(this).text();
+        var input = $('.combobox-input');
+        input.val(text);
+    }
 
-	function buildInput() {
-		$('<input>').addClass('form-control combobox-input')
-		.appendTo(comboboxParent);
-	}
+    function buildInput() {
+        $('<input>').addClass('form-control combobox-input')
+        .appendTo(comboboxParent);
+    }
 
-	function buildCaret() {
-		return $('<span data-toggle="dropdown">').addClass('dropdown-toggle')
-		.append($('<span>').addClass('caret'))
-		.appendTo(comboboxParent);
-	}
+    function buildCaret() {
+        return $('<span data-toggle="dropdown">').addClass('dropdown-toggle')
+        .append($('<span>').addClass('caret'))
+        .appendTo(comboboxParent);
+    }
 
-	function onToggleClick() {
-		$('input.combobox-input').focus();
-	}
+    function onToggleClick() {
+        $('input.combobox-input').focus();
+    }
 
-	function onArrowKeyDown(e) {
-		var menu = $('.dropdown-menu');
-		if (e.keyCode === 40) {
-			if (document.activeElement === $('input')[0]) {
-				$('li[tabindex="1"]').focus();
-			} else {
-				$(document.activeElement).next().focus();
-			}
-		} else if (e.keyCode === 38) {
-			$(document.activeElement).prev().focus();
-		} else if (e.keyCode === 27) {
-			menu.toggle();
-		}
-	}
+    function onArrowKeyDown(e) {
+        var menu = $('.dropdown-menu');
+        if (e.keyCode === 40) {
+            if (document.activeElement === $('input')[0]) {
+                $('li[tabindex="1"]').focus();
+            } else {
+                $(document.activeElement).next().focus();
+            }
+        } else if (e.keyCode === 38) {
+            $(document.activeElement).prev().focus();
+        } else if (e.keyCode === 27) {
+            menu.removeClass('open');
+        }
+    }
 
-	$.fn.combobox = function(options) {
-		buildInput();
-		buildCaret().click(onToggleClick);
-		$('body').keydown(onArrowKeyDown);
-		buildOptionList(options);
-	};
+    $.fn.combobox = function(options) {
+        buildInput();
+        buildCaret().click(onToggleClick);
+        $('body').keydown(onArrowKeyDown);
+        buildOptionList(options);
+    };
 
 }( window.jQuery ));
